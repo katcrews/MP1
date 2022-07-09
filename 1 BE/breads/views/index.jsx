5bdbd9a277cfe4a16c8ae13a61f2d217ea6b97b6
Kat Crews <katfreitas808@gmail.com>
2022-07-09 13:44:25 -0700

    create and new routes complete

--- 1 BE/breads/views/index.jsx
+++ 1 BE/breads/views/index.jsx
@@ -8,18 +8,22 @@ function Index({ breads, title }) {
             {/* <p>I have {breads[0].name} bread!</p> */}
             {/* This is a JSX comment. */}
             <ul>
-            {
-                breads.map((bread, index)=> {
-                    return (
-                        <li key={index}>
-                            <a href={`/breads/${index}`}>
-                            {bread.name}
-                            </a>
-                        </li>
-                    )
-                })
-            }
+                {
+                    breads.map((bread, index) => {
+                        return (
+                            <li key={index}>
+                                <a href={`/breads/${index}`}>
+                                    {bread.name}
+                                </a>
+                            </li>
+                        )
+                    })
+                }
             </ul>
+            <div className="newButton">
+                <a href="/breads/new"><button>Add a new bread</button></a>
+            </div>
+
         </Default>
 
     )
