39efbc35636eb5c6257b31076f5cbf753852358a
Kat Crews <katfreitas808@gmail.com>
2022-07-09 10:58:23 -0700

    sending index and show data

--- 1 BE/breads/controllers/breads_controller.js
+++ 1 BE/breads/controllers/breads_controller.js
@@ -14,9 +14,14 @@ breads.get('/', (req, res) => {
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-  res.send(Bread[req.params.arrayIndex])
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.render('404')
+  }
 })
-
 
 module.exports = breads
 
